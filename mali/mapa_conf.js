c0f9b87d8acfe6062f6ca08634ac76077f5e7569
luisoscorima <astroscorima@gmail.com>
2025-05-06 17:34:20 -0500

    Add configuration file for MALI contact links and update marker image
    - Created `mapa_conf.js` to store URLs for brochures, images, and contact information.
    - Added links for various locations and virtual services.
    - Included image paths for visual elements on the contact page.
    - Introduced a new `marker.png` image file for use in the application.

---
+++ mali/mapa_conf.js
@@ -0,0 +1,25 @@
+// Este archivo contiene la configuración de los enlaces y contactos para la página de contacto del MALI.
+const URLS = {
+    BROCHURES: {
+      PRINCIPAL: 'https://bit.ly/Horarios-Principal',
+      LA_MOLINA: 'https://bit.ly/Horarios-Camacho',
+      SAN_MIGUEL: 'https://bit.ly/Horarios-SanMiguel1',
+      CHORRILLOS: 'https://bit.ly/Horarios-Chorrillos',
+      LOS_OLIVOS: 'https://bit.ly/Horarios-VillaSol',
+      SJL: 'https://bit.ly/Horarios-Arabiscos',
+      PUEBLO_LIBRE: 'https://bit.ly/Horarios-PuebloLibre',
+      VIRTUAL: 'https://bit.ly/Virtual-Abril'
+    },
+    IMAGES: {
+      RECTANGULO: '../mali/img/Rectangulo.png',
+      WHATSAPP: '../mali/img/WhatsApp.png',
+      CORREO: '../mali/img/correo.png',
+    },
+    CONTACTOS: {
+      WHATSAPP: '961 505 576',
+      TELEFONO: '(01) 204 0000 (Anexo 1)',
+      EMAIL: 'cursosdearte@mali.pe',
+      EMAIL_VIRTUAL: 'cursosvirtuales@mali.pe',
+      SOPORTE_VIRTUAL: '946 216 569'
+    }
+  };
